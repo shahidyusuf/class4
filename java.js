118506ad314a41de0fc2dac5c729e135d07ff7e8
Shahid Yusuf <shd.92yus@gmail.com>
2023-02-15 21:08:23 +0500

    update.shop

---
+++ java.js
@@ -0,0 +1,17 @@
+// for (var i = 0; i <= 99; i++) {
+    // console.log("From Loop");
+    // console.log("From Loop" +i)
+// }
+
+// PRINT TABLE
+
+// for (i=1; i<=10; i++) {
+    // console.log("2 x" +i+ "=" +i*2);
+// }
+
+// prompt
+
+var table = prompt("Write a number.") 
+for (var i = 1; i <= 10; i++){
+console.log(table + "x" + i + "=" + i*table);
+}
